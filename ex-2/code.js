0e2559097e7fe70f9bd56fc4db4c07d9ca364b94
njanth <pree.janthasom@hotmail.com>
2022-06-10 16:42:07 +0200

    Make two separate exercise folders

---
+++ ex-2/code.js
@@ -0,0 +1,14 @@
+let counter= 0;
+
+function btn(){
+    console.log("click");
+    counter++;
+    if(counter == 5){
+        counter = 0;
+    }
+    let image = document.getElementById('image');
+    let images = ['bild1.jpg','bild2.jpg','bild3.jpg','bild4.jpg','bild5.jpg', ];
+
+    image.src = images [counter];
+
+    }
