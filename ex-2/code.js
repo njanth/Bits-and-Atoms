23b7e5f6607126072fce13037f07507b35638b02
njanth <pree.janthasom@hotmail.com>
2022-06-10 16:56:00 +0200

    Add main page
    Select homework from that main page

--- ex-2/code.js
+++ ex-2/code.js
@@ -1,14 +1,14 @@
-let counter= 0;
+let counter = 0;
 
-function btn(){
+function btn() {
     console.log("click");
     counter++;
-    if(counter == 5){
+    if (counter == 5) {
         counter = 0;
     }
     let image = document.getElementById('image');
-    let images = ['bild1.jpg','bild2.jpg','bild3.jpg','bild4.jpg','bild5.jpg', ];
+    let images = ['bild1.jpg', 'bild2.jpg', 'bild3.jpg', 'bild4.jpg', 'bild5.jpg',];
 
-    image.src = images [counter];
+    image.src = images[counter];
 
-    }
+}
